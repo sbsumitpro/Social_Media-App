5c88bb99e8451a9c89f8a3ca2aeacf4da7dc38fe
Sumit Biswas <sbsumitpro@gmail.com>
2023-11-13 16:55:24 +0530

    Added new mongoose config code

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,10 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI);
-
-const db =mongoose.connection;
-
-db.on("error", console.log.bind(console,"error connecting to DB"))
-db.once("open", function(){
-    console.log("Successfully connected to the DB");
-})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Successfully connected to the database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error.message);
+  });
