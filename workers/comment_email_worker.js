b60ea7e86845af63ee28831018bba4c543f36b8b
Sumit Biswas <sbsumitpro@gmail.com>
2023-11-13 17:21:55 +0530

    removed redis

--- workers/comment_email_worker.js
+++ workers/comment_email_worker.js
@@ -1,12 +1,12 @@
-const queue = require("../config/kue");
-const commentsMailer = require("../mailers/comments_mailer");
+// const queue = require("../config/kue");
+// const commentsMailer = require("../mailers/comments_mailer");
 
-queue.process("emails",(job, done)=>{
+// queue.process("emails",(job, done)=>{
 
-    console.log("Emails worker is processing a job", job.data);
+//     console.log("Emails worker is processing a job", job.data);
 
-    // Now we are sending the email from the queue..
-    commentsMailer.newComment(job.data);
+//     // Now we are sending the email from the queue..
+//     commentsMailer.newComment(job.data);
 
-    done();
-})
+//     done();
+// })
